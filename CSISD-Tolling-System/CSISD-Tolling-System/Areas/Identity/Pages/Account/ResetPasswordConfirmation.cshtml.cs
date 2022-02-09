28c8c9a909dfc673b392766b2991e9ac158dfebe
Joe Smithurst <josephsmithurst@gmail.com>
2022-02-09 12:46:52 +0000

    Changed the login page to match the UI wireframe

---
+++ CSISD-Tolling-System/CSISD-Tolling-System/Areas/Identity/Pages/Account/ResetPasswordConfirmation.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSISD_Tolling_System.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ResetPasswordConfirmationModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}

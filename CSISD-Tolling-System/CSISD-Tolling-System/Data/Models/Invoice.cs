026c0cef126420d6386c6dc83a182d01d241c6c2
Barney Wilks <barneywilks1@gmail.com>
2022-02-15 14:38:43 +0000

    Add constructor to invoice (as per static model) with unit tests

--- CSISD-Tolling-System/CSISD-Tolling-System/Data/Models/Invoice.cs
+++ CSISD-Tolling-System/CSISD-Tolling-System/Data/Models/Invoice.cs
@@ -10,11 +10,31 @@ namespace CSISD_Tolling_System.Models
     {
         [Key]
         public long Id { get; set; }
+
+        /// <summary>
+        /// Uses decimal instead of double, owing to floating point
+        /// not being able to accurately represent the base 10 multiples
+        /// that we use for money.
+        ///
+        /// References:
+        ///     https://stackoverflow.com/a/3730040
+        ///     https://docs.microsoft.com/en-us/dotnet/api/system.decimal?view=net-6.0#remarks
+        /// </summary>
         [Required]
-        public double Fee { get; set; }
+        public decimal Fee { get; set; }
+
         [Required, StringLength(450)]
         public string UserId { get; set; }
+
         [Required]
         public int VehicleId { get; set; }
+
+        public Invoice(long id, decimal fee, string userid, int vehicleid)
+        {
+            this.Id = id;
+            this.Fee = fee;
+            this.UserId = userid;
+            this.VehicleId = vehicleid;
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # CSISD-Toll-System
 
-![CI](https://github.com/b7011343/CSISD-Toll-System/actions/workflows/ci.yml/badge.svg)
+![CI](https://github.com/b7011343/CSISD-Toll-System/actions/workflows/<WORKFLOW_FILE>/badge.svg)
 
 Run this line to call the first migration in the package manager console:
 
+```
   update-database -context ApplicationDbContext -migration 00000000000000_CreateIdentitySchema
+```

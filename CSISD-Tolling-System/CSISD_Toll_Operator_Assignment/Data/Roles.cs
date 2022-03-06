733eec35d5cc8711e82474009b94210c140181c3
Barney Wilks <barneywilks1@gmail.com>
2022-03-06 15:41:45 +0000

    Simplify HomeController.Index a bit & add Roles utility class
    Roles is just for replacing the magic string constants used to represent roles

---
+++ CSISD-Tolling-System/CSISD_Toll_Operator_Assignment/Data/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace CSISD_Toll_Operator_Assignment.Data
+{
+    public class Roles
+    {
+        public const string RoadUser      = "road-user";
+        public const string TollOperator  = "toll-operator";
+        public const string Administrator = "admin";
+    }
+}

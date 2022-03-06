554ac5954e8ab17f35c427b2d3dd4c6c71899bec
Barney Wilks <barneywilks1@gmail.com>
2022-03-06 18:08:57 +0000

    Clean up IApplicationDbContext
    Interface members are implicitly public

--- CSISD-Tolling-System/CSISD_Toll_Operator_Assignment/Data/IApplicationDbContext.cs
+++ CSISD-Tolling-System/CSISD_Toll_Operator_Assignment/Data/IApplicationDbContext.cs
@@ -1,20 +1,16 @@
 ﻿using CSISD_Toll_Operator_Assignment.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CSISD_Toll_Operator_Assignment.Data
 {
     public interface IApplicationDbContext
     {
-        public DbSet<Card> Cards                   { get; set; }
-        public DbSet<Contract> Contracts           { get; set; }
-        public DbSet<Invoice> Invoices             { get; set; }
-        public DbSet<PaymentMethod> PaymentMethods { get; set; }
-        public DbSet<Preference> Preferences       { get; set; }
-        public DbSet<RFID> RFIDs                   { get; set; }
-        public DbSet<Vehicle> Vehicles             { get; set; }
+        DbSet<Card> Cards                   { get; set; }
+        DbSet<Contract> Contracts           { get; set; }
+        DbSet<Invoice> Invoices             { get; set; }
+        DbSet<PaymentMethod> PaymentMethods { get; set; }
+        DbSet<Preference> Preferences       { get; set; }
+        DbSet<RFID> RFIDs                   { get; set; }
+        DbSet<Vehicle> Vehicles             { get; set; }
     }
 }

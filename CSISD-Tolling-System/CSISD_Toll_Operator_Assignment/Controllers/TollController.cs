fe0875cd22bff5c3ef7e8fac62d5ad56815cc9ff
Barney Wilks <barneywilks1@gmail.com>
2022-03-14 16:11:47 +0000

    Formatting changes to TollController

--- CSISD-Tolling-System/CSISD_Toll_Operator_Assignment/Controllers/TollController.cs
+++ CSISD-Tolling-System/CSISD_Toll_Operator_Assignment/Controllers/TollController.cs
@@ -16,13 +16,6 @@ namespace CSISD_Toll_Operator_Assignment.Controllers
 {
     public class TollController : Controller
     {
-        //private readonly ILogger<TollController> logger;
-        private readonly ApplicationDbContext db;
-        private readonly UserManager<User> userManager;
-        private readonly SignInManager<User> signInManager;
-        private readonly InvoiceService invoiceService;
-        private readonly PaymentProcessingSimulationService paymentService;
-        //this method instantiates the variables required for the controller
         private readonly ApplicationDbContext _db;
         private readonly UserManager<User>    _userManager;
         private readonly InvoiceService       _invoiceService;
@@ -33,69 +26,89 @@ namespace CSISD_Toll_Operator_Assignment.Controllers
             _db             = db;
             _invoiceService = invoiceService;
         }
-        //this method adds the selected invoice (specified by the parameter invoiceId), list of cards linked to the signed in user and the vehicle linked to the invoice to the payment page
+
+        /// <summary>
+        /// Adds the selected invoice (specified by the parameter invoiceId),
+        /// list of cards linked to the signed in user and the vehicle linked to the invoice to the payment page
+        /// </summary>
         [HttpGet]
         [AutoValidateAntiforgeryToken]
         [Authorize(Roles = Roles.RoadUser)]
         public IActionResult Payment(long invoiceId)
         {
-            // Generate view model and return view
-            //get the required invoice by specifying the invoiceId from the ApplicationdbContext
-            Invoice invoice = db.Invoices.Where(x => x.Id == invoiceId).First();
-            //get the list of cards that are linked to the logged in user from the ApplicationdbContext
-            List<Card> cards = db.Cards.Where(x => x.OwnerID == userManager.GetUserId(User)).ToList();
-            //get the vehicle linked to the invoice from the ApplicationdbContext
-            Vehicle vehicle = db.Vehicles.Where(x => x.Id == invoiceId).First();
-            //add objects above the PaymentViewModel
+            // Get the required invoice by specifying the invoiceId from the ApplicationDbContext
+            Invoice invoice = _db.Invoices.Where(x => x.Id == invoiceId).First();
+
+            // Get the list of cards that are linked to the logged in user from the ApplicationDbContext
+            List<Card> cards = _db.Cards.Where(x => x.OwnerID == _userManager.GetUserId(User)).ToList();
+
+            // Get the vehicle linked to the invoice from the ApplicationdbContext
+            Vehicle vehicle = _db.Vehicles.Where(x => x.Id == invoiceId).First();
+
+            // Add objects above the PaymentViewModel
             PaymentViewModel model = new PaymentViewModel()
             {
                 invoice = invoice,
                 cards = cards,
                 vehicle = vehicle
             };
-            //pass model through View()
+
+            // Pass model through View()
             return View(model);
         }
-        //this method validates the cvv and allows the user to pay their invoices
+
+        /// <summary>
+        /// This method validates the CVV and allows the user to pay their invoices.
+        /// </summary>
         [HttpPost]
         [AutoValidateAntiforgeryToken]
         [Authorize(Roles = Roles.RoadUser)]
         public IActionResult Payment()
         {
-            // Allows user to pay for an invoice
-            //get the required invoice by specifying the invoiceId from the ApplicationdbContext
-            Invoice invoice = db.Invoices.Where(x => x.Id == (long)Convert.ToDouble(Request.Form["id"])).First();
-            //get the card selected by the user in the payment form
-            Card card = db.Cards.Where(x => x.CardNumber == (string)Request.Form["cards"].ToString()).First();
-            //get the list of cards that are linked to the logged in user from the ApplicationdbContext
-            List<Card> cards = db.Cards.Where(x => x.OwnerID == userManager.GetUserId(User)).ToList();
-            //get the vehicle linked to the invoice from the ApplicationdbContext
-            Vehicle vehicle = db.Vehicles.Where(x => x.Id == invoice.Id).First();
-            //add objects above the PaymentViewModel
+            // Get the required invoice by specifying the invoiceId from the ApplicationdbContext
+            Invoice invoice = _db.Invoices.Where(x => x.Id == (long)Convert.ToDouble(Request.Form["id"])).First();
+
+            // Get the card selected by the user in the payment form
+            Card card = _db.Cards.Where(x => x.CardNumber == (string)Request.Form["cards"].ToString()).First();
+
+            // Get the list of cards that are linked to the logged in user from the ApplicationdbContext
+            List<Card> cards = _db.Cards.Where(x => x.OwnerID == _userManager.GetUserId(User)).ToList();
+
+            // Get the vehicle linked to the invoice from the ApplicationdbContext
+            Vehicle vehicle = _db.Vehicles.Where(x => x.Id == invoice.Id).First();
+
+            // Add objects above the PaymentViewModel
             PaymentViewModel model = new PaymentViewModel()
             {
                 invoice = invoice,
-                cards = cards,
+                cards   = cards,
                 vehicle = vehicle
             };
+
             int cvv = Convert.ToInt32(Request.Form["cvv"]);
-            //cvv validation - checking if the user input cvv is the same as the cvv on the Card in the database
+
+            // Check if the user input CVV is the same as the CVV on the card in the database
             if(cvv == card.Cvv)
             {
-                //change boolean Paid from false to true
+                // Change boolean Paid from false to true
                 invoice.Paid = true;
-                //save changes and update the context state
-                db.SaveChanges();
-                //return user to the home index page
+
+                // Save changes and update the context state
+                _db.SaveChanges();
+
+                // Return user to the home index page
                 return LocalRedirect("/Home/Index");
             }
             else
             {
-                //if the user input cvv does not match the card cvv in the database then return an error message page to the user
+                // If the user input CVV does not match the card CVV in the database then return an error message page to the user
                 return Ok("The card details you have entered are incorrect, please go back and enter them correctly.");
             }
         }
-        //this method takes user input and adds a new card
+
+        /// <summary>
+        /// This method takes user input and adds a new card
+        /// </summary>
         [HttpPost]
         [Authorize(Roles = "road-user")]
         [ValidateAntiForgeryToken]
@@ -110,19 +123,23 @@ namespace CSISD_Toll_Operator_Assignment.Controllers
             Card card = new Card()
             {
                 CardNumber = model.CardNumber,
-                Cvv = model.CVV,
+                Cvv        = model.CVV,
                 ExpiryDate = model.ExpiryDate,
                 NameOnCard = model.NameOnCard,
-                OwnerID = _userManager.GetUserId(User)
+                OwnerID    = _userManager.GetUserId(User)
             };
-            //add card to Cards table in ApplicationDbContext, then save the changes
-            db.Cards.Add(card);
-            db.SaveChanges();
+
+            // Add card to Cards table in ApplicationDbContext, then save the changes
+            _db.Cards.Add(card);
+            _db.SaveChanges();
 
             return RedirectToAction("Index", "Home");
         }
-        //this method redirects the user to the AddCard view and this is only seen by the road-user
-        //the [Authorize] allows you to specify which user roles can see certain views
+
+        /// <summary>
+        /// This method redirects the user to the AddCard view and this is only seen by the road-user
+        /// the [Authorize] allows you to specify which user roles can see certain views
+        /// </summary>
         [HttpGet]
         [Authorize(Roles = "road-user")]
         public IActionResult AddCard()
@@ -130,50 +147,67 @@ namespace CSISD_Toll_Operator_Assignment.Controllers
             // Returns AddCard view
             return View();
         }
-        //this methods shows the Contracts view and allows the road-user and toll-operator to view it
+
+        /// <summary>
+        /// This methods shows the Contracts view and allows the road-user and toll-operator to view it
+        /// </summary>
         [HttpGet]
         [Authorize(Roles = "road-user, toll-operator")]
         public async Task<IActionResult> Contracts()
         {
             // Gets current user and checks role
             string userEmail = HttpContext.User.Identity.Name;
-            User user = await userManager.FindByEmailAsync(userEmail);
-            string role = (await userManager.GetRolesAsync(user)).First();
-            //get list of all contracts
-            List<Contract> _tollContracts = db.Contracts.ToList();
-            var id = userManager.GetUserId(User);
-            //get list of contracts linked to the user that is signed in
-            List<Contract> _userContracts = db.Contracts.Where(x => x.UserId == userManager.GetUserId(User)).ToList();
-            //create ContractViewModel for the toll operator
-            ContractViewModel tollContracts = new ContractViewModel()
-            {
-                Contracts = _tollContracts
-            };
-            //create ContractViewModel for the road user
-            ContractViewModel userContracts = new ContractViewModel()
-            {
-                Contracts = _userContracts
-            };
+
+            User   user = await _userManager.FindByEmailAsync(userEmail);
+            string role = (await _userManager.GetRolesAsync(user)).First();
+
+            // Get list of all contracts
+            List<Contract> _tollContracts = _db.Contracts.ToList();
+
+            string id = _userManager.GetUserId(User);
+
+            // Get list of contracts linked to the user that is signed in
+            List<Contract> _userContracts = _db.Contracts.Where(x => x.UserId == _userManager.GetUserId(User)).ToList();
+
             // Different view rendered dependent on role
             switch (role)
             {
                 case Roles.RoadUser:
+                    ContractViewModel userContracts = new ContractViewModel()
+                    {
+                        Contracts = _userContracts
+                    };
+
                     return View("ContractRoadUser", userContracts);
+
                 case Roles.TollOperator:
+                    ContractViewModel tollContracts = new ContractViewModel()
+                    {
+                        Contracts = _tollContracts
+                    };
+
                     return View("ContractTollOperator", tollContracts);
             }
+
             return View();
         }
-        //this method shows the ContactDetails view
+
+        /// <summary>
+        /// This method shows the ContactDetails view
+        /// </summary>
         [HttpGet]
         [Authorize(Roles = "road-user, toll-operator")]
         public IActionResult ContractDetails(long contractID)
         {
             // Get the contract required, and return view with that contract as a model
             Contract contract = _db.Contracts.Where(x => x.Id == contractID).First();
+
             return View(contract);
         }
-        //this method shows the Tollhistory view
+
+        /// <summary>
+        /// This method shows the toll history view
+        /// </summary>
         public async Task<IActionResult> TollHistory()
         {
             string userEmail = HttpContext.User.Identity.Name;
@@ -182,8 +216,8 @@ namespace CSISD_Toll_Operator_Assignment.Controllers
                 return View();
 
             // Gets current user and role
-            User user = await _userManager.FindByEmailAsync(userEmail);
-            string role = (await _userManager.GetRolesAsync(user)).First();
+            User                 user  = await _userManager.FindByEmailAsync(userEmail);
+            string               role  = (await _userManager.GetRolesAsync(user)).First();
             TollHistoryViewModel model = new TollHistoryViewModel(role, user, _invoiceService);
 
             // Renders different view dependent on roll for toll history
